7f71399aa4edf8a32ed57821d7c227f342912316
Collin Argo <collinargo@gmail.com>
2019-01-08 12:18:42 -0500

    add friendship schema

--- models/Users.js
+++ models/Users.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+const FriendShipSchema = require('./FriendShip')
+
 const UserSchema = new Schema({
     name: String,
     pwhash: String,
-    userType: String
+    userType: String,
+    friends: [FriendShipSchema]
 })
 
 const User = mongoose.model('User', UserSchema)
