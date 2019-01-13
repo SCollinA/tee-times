291efb4f5bb11ae5a2e4be64e72097d9c6340a08
Collin Argo <collinargo@gmail.com>
2019-01-13 12:15:30 -0500

    use buffer for profile image

--- index.js
+++ index.js
@@ -219,7 +219,7 @@ app.post('/updateUser', checkUser, (req, res, next) => {
                     name: newUsername,
                     // either use the new hash password or the old one if not updated
                     pwhash: pwhash,
-                    picture: newPicture
+                    picture: Buffer.from(newPicture)
                 }
             ).then(() => {
                 User.findById(updatingUser._id)
