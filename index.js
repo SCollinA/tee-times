32a6225ca7cecf08281f54ce68e3b6a2e000d482
Collin Argo <collinargo@gmail.com>
2019-01-15 15:48:55 -0500

    make secure false

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(session({
     cookie: {
       maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
       httpOnly: true, 
-      secure: true,
+      secure: false,
     },
     store,
     resave: false,
