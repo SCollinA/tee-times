84a17d2dcd0f469e04e00e3176b75ac982198604
Evan Procter <evanprocter@gmail.com>
2019-01-16 13:59:39 -0500

    added build folder

---
+++ build/precache-manifest.289bb6879d420ecf6b56e57029b09a65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6bf67772264fcfeb6c52611f521d776",
+    "url": "/static/media/12th-hole.b6bf6777.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "668d58f951c5ec7d7191",
+    "url": "/static/js/main.668d58f9.chunk.js"
+  },
+  {
+    "revision": "476684a8faae0ad78fb8",
+    "url": "/static/js/1.476684a8.chunk.js"
+  },
+  {
+    "revision": "668d58f951c5ec7d7191",
+    "url": "/static/css/main.870b18cc.chunk.css"
+  },
+  {
+    "revision": "b567655d7bb8786e613e3f763316fd3d",
+    "url": "/index.html"
+  }
+];
